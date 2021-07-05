39b73e60d9f9fe840078d921359872e980190baf
kim <ing03201@gmail.com>
2021-07-05 19:15:13 +0900

    add flask app

--- app.py
+++ app.py
@@ -1,19 +1,82 @@
+"""
+
+A small Test application to show how to use Flask-MQTT.
+
+"""
+import logging
+
+import eventlet
+import json
 from flask import Flask, render_template
+from flask_mqtt import Mqtt
+from flask_socketio import SocketIO
+from flask_bootstrap import Bootstrap
+
+eventlet.monkey_patch()
+
 app = Flask(__name__)
+app.config['SECRET'] = 'my secret key'
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['MQTT_BROKER_URL'] = 'localhost'
+app.config['MQTT_BROKER_PORT'] = 1883
+app.config['MQTT_CLIENT_ID'] = 'flask_mqtt'
+app.config['MQTT_CLEAN_SESSION'] = True
+app.config['MQTT_USERNAME'] = ''
+app.config['MQTT_PASSWORD'] = ''
+app.config['MQTT_KEEPALIVE'] = 5
+app.config['MQTT_TLS_ENABLED'] = False
+app.config['MQTT_LAST_WILL_TOPIC'] = 'home/lastwill'
+app.config['MQTT_LAST_WILL_MESSAGE'] = 'bye'
+app.config['MQTT_LAST_WILL_QOS'] = 2
 
-@app.route("/") #route 데코레이터 - 어떤 URL이 펑션을 호출할지 알려준다.
-@app.route("/index")#두가지 URL를 적어도 되네...
-def main_index():
-	return render_template('main.html')
+# Parameters for SSL enabled
+# app.config['MQTT_BROKER_PORT'] = 8883
+# app.config['MQTT_TLS_ENABLED'] = True
+# app.config['MQTT_TLS_INSECURE'] = True
+# app.config['MQTT_TLS_CA_CERTS'] = 'ca.crt'
 
-@app.route("/broadcast")
-def broadcast():
-	return render_template('broadcast.html')
+mqtt = Mqtt(app)
+socketio = SocketIO(app)
+bootstrap = Bootstrap(app)
 
-@app.route("/board")
-def raspberrypi():
-	return render_template('board.html')
-  
-if __name__ == '__main__':  # 모듈이 실행 됨을 알림
-   app.run('0.0.0.0',port=5000,debug=True)
-   # 서버 실행, 파라미터로 debug 여부, port 설정 가능
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@socketio.on('publish')
+def handle_publish(json_str):
+    data = json.loads(json_str)
+    mqtt.publish(data['topic'], data['message'], data['qos'])
+
+
+@socketio.on('subscribe')
+def handle_subscribe(json_str):
+    data = json.loads(json_str)
+    mqtt.subscribe(data['topic'], data['qos'])
+
+
+@socketio.on('unsubscribe_all')
+def handle_unsubscribe_all():
+    mqtt.unsubscribe_all()
+
+
+@mqtt.on_message()
+def handle_mqtt_message(client, userdata, message):
+    data = dict(
+        topic=message.topic,
+        payload=message.payload.decode(),
+        qos=message.qos,
+    )
+    socketio.emit('mqtt_message', data=data)
+
+
+@mqtt.on_log()
+def handle_logging(client, userdata, level, buf):
+    # print(level, buf)
+    pass
+
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000, use_reloader=False, debug=True)
