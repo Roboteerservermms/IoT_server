2d76d9eebb75031e075191b451374e09c71d5894
kim <ing03201@gmail.com>
2021-08-17 12:56:46 +0900

    add client class

--- client.py
+++ client.py
@@ -1,25 +1,65 @@
+from logging import error
 import socket
+from datetime import datetime
+import json
 
-class Client:
-	'''
-	args client 연결 옵션
-	'''
-	def __init__(self, host, port):
-		self.client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-		
-	def connect(self, host, port):
-		try:
-			self.client_socket.connect((host,port))
-		except:
-			print("Error: cannot connect to server")
-			return 
-	def disconnect(self):
-		self.client_socket.close()
-		
-	def send(self, filename, data):
-		self.client_socket.sendall(filename.encode())
-		reSize = self.client_socket.recv(1024)
+class TCPClient:
+    ''' A simple TCP Client that uses IPv4 '''
 
-	def receive(self):
-		self.rbuff = self.client_socket.recv(1024)
-		return str(self.rbuff, encoding='utf-8')
+    def __init__(self, host, port):
+        self.host = host        # host address
+        self.port = port        # host port
+        self.conn_sock = None   # connection socket
+
+    def printwt(self, msg):
+        ''' Print message with current date and time '''
+
+        current_date_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print(f'[{current_date_time}] {msg}')
+
+    def create_socket(self):
+        ''' Create a socket that uses IPv4 and TCP '''
+
+        self.printwt('Creating connection socket ...')
+        self.conn_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.printwt('Socket created')
+
+    def interact_with_server(self, msg=None):
+        ''' Connect and interact with a TCP Server. '''
+
+        try:
+            # connect to server
+            self.printwt(f'Connecting to server [{self.host}] on port [{self.port}] ...')
+            self.conn_sock.connect((self.host, self.port))
+
+            # send data
+            self.printwt('Sending msg to server to get phone number ...')
+            if not msg:
+                data_dic = {
+                    "GPIO_IN": 65,
+                    "GPIO_OUT": 111,
+                    "data": "video.mp4"
+                }
+                msg = json.dumps(data_dic)
+            self.conn_sock.sendall(msg.encode('utf-8'))
+            self.printwt('[ SENT ]')
+            print('\n', msg, '\n')
+        except OSError as err:
+            self.printwt('Cannot connect to server')
+            print(err)
+
+        finally:
+            # close socket
+            self.printwt('Closing connection socket...')
+            self.conn_sock.close()
+            self.printwt('Socket closed')
+
+def main():
+    ''' Create a TCP Client and interact with the server at 127.0.0.1:4444'''
+
+    tcp_client = TCPClient('127.0.0.1', 4444)
+    tcp_client.create_socket()
+    tcp_client.interact_with_server()
+
+if __name__ == '__main__':
+    main()
