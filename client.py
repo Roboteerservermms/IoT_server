9ed788e427ba63ee285e9d5069b3879b8ba47feb
kim <ing03201@gmail.com>
2021-08-17 17:02:05 +0900

    change udp

--- client.py
+++ client.py
@@ -3,8 +3,8 @@ import socket
 from datetime import datetime
 import json
 
-class TCPClient:
-    ''' A simple TCP Client that uses IPv4 '''
+class UDPClient:
+    ''' A simple UDP Client that uses IPv4 '''
 
     def __init__(self, host, port):
         self.host = host        # host address
@@ -18,48 +18,30 @@ class TCPClient:
         print(f'[{current_date_time}] {msg}')
 
     def create_socket(self):
-        ''' Create a socket that uses IPv4 and TCP '''
+        ''' Create a socket that uses IPv4 and UDP '''
 
         self.printwt('Creating connection socket ...')
-        self.conn_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.conn_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.printwt('Socket created')
 
-    def interact_with_server(self, msg=None):
-        ''' Connect and interact with a TCP Server. '''
-
+    def interact_with_server(self, data_dic=None):
+        ''' Connect and interact with a UDP Server. '''
         try:
-            # connect to server
-            self.printwt(f'Connecting to server [{self.host}] on port [{self.port}] ...')
-            self.conn_sock.connect((self.host, self.port))
-
-            # send data
-            self.printwt('Sending msg to server to get phone number ...')
-            if not msg:
-                data_dic = {
-                    "GPIO_IN": 65,
-                    "GPIO_OUT": 111,
-                    "data": "video.mp4"
-                }
-                msg = json.dumps(data_dic)
-            self.conn_sock.sendall(msg.encode('utf-8'))
+            msg = json.dumps(data_dic)
+            self.conn_sock.sendto(bytes(msg, "utf-8"), (self.host,self.port))
             self.printwt('[ SENT ]')
             print('\n', msg, '\n')
         except OSError as err:
             self.printwt('Cannot connect to server')
             print(err)
 
-        finally:
-            # close socket
-            self.printwt('Closing connection socket...')
-            self.conn_sock.close()
-            self.printwt('Socket closed')
-
 def main():
-    ''' Create a TCP Client and interact with the server at 127.0.0.1:4444'''
+    ''' Create a UDP Client and interact with the server at 127.0.0.1:4444'''
 
-    tcp_client = TCPClient('127.0.0.1', 4444)
+    tcp_client = UDPClient('127.0.0.1', 8080)
     tcp_client.create_socket()
-    tcp_client.interact_with_server()
+    tcp_client.interact_with_server(data_dic={"GPIO_IN": 65, "GPIO_OUT": 111,"data": "video.mp4"})
+
 
 if __name__ == '__main__':
     main()
