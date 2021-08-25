f277422dc48da9c8b77a426cb673ae63cb02ea7b
kim <ing03201@gmail.com>
2021-08-25 14:28:04 +0900

    make  simple server

--- app.py
+++ app.py
@@ -1,85 +1,45 @@
-"""
-
-A small Test application to show how to use Flask-MQTT.
-
-"""
-import logging
-
-import eventlet
-import json
 from flask import Flask, render_template, request
-from flask_mqtt import Mqtt
-from flask_socketio import SocketIO
-from flask_bootstrap import Bootstrap
 from werkzeug.utils import secure_filename
-import os
-eventlet.monkey_patch()
-
+import os, subprocess
+from client import UDPClient
 app = Flask(__name__)
-app.config['SECRET'] = 'my secret key'
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config['MQTT_BROKER_URL'] = 'localhost'
-app.config['MQTT_BROKER_PORT'] = 1883
-app.config['MQTT_USERNAME'] = ''
-app.config['MQTT_PASSWORD'] = ''
-app.config['MQTT_KEEPALIVE'] = 5
-app.config['MQTT_TLS_ENABLED'] = False
+client = UDPClient()
+client.create_socket()
+#app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 #파일 업로드 용량 제한 단위:바이트
 
-# Parameters for SSL enabled
-# app.config['MQTT_BROKER_PORT'] = 8883
-# app.config['MQTT_TLS_ENABLED'] = True
-# app.config['MQTT_TLS_INSECURE'] = True
-# app.config['MQTT_TLS_CA_CERTS'] = 'ca.crt'
-
-mqtt = Mqtt(app)
-socketio = SocketIO(app)
-bootstrap = Bootstrap(app)
-
-
+#HTML 렌더링
 @app.route('/')
-def index():
-    return render_template('index.html')
-    
-@app.route('/fileupload', methods=['POST'])
-def file_upload():
-    file = request.files['file']
-    filename = secure_filename(file.filename)
-    os.makedirs(image_path, exists_ok=True)
-    file.save(os.path.join(image_path, filename))
-    return
+def home_page():
+    return render_template('home.html')
 
-@socketio.on('publish')
-def handle_publish(json_str):
-    data = json.loads(json_str)
-    print(data)
-    mqtt.publish(data['topic'], data['data'], data['qos'])
+# 파일 리스트
+@app.route('/list')
+def list_page():
+    file_list = os.listdir("./uploads")
+    html = """<center><a href="/">홈페이지</a><br><br>"""
+    html += "file_list: {}".format(file_list) + "</center>"
+    return html
 
+#업로드 HTML 렌더링
+@app.route('/upload')
+def upload_page():
+    return render_template('upload.html')
 
-@socketio.on('subscribe')
-def handle_subscribe(json_str):
-    data = json.loads(json_str)
-    mqtt.publish(data['topic'], data['data'], data['qos'])
+#파일 업로드 처리
+@app.route('/<ip>/fileUpload', methods = ['GET', 'POST'])
+def upload_file(ip):
+    if request.method == 'POST':
+        f = request.files['file']
+        out =  int(request.form.getlist("GPIOOUT")[0])
+        i = int(request.form.getlist("GPIOIN"))
+        data_dic={"GPIO_IN": i, "GPIO_OUT": out,"data": f"{secure_filename(f.filename)}"}
+        print(f"sendto {ip} {data_dic}")
+        client.interact_with_server(ip, 8080, data_dic)
+        #저장할 경로 + 파일명
+        f.save('./uploads/' + secure_filename(f.filename))
+        subprocess.getoutput(f"sshpass -p orangepi scp {secure_filename(f.filename)} orangepi@{ip}:/home/orangepi/IoT_target/ ")
+    return render_template('check.html')
 
-
-@socketio.on('unsubscribe_all')
-def handle_unsubscribe_all():
-    mqtt.unsubscribe_all()
-
-
-@mqtt.on_message()
-def handle_mqtt_message(client, userdata, message):
-    data = dict(
-        topic=message.topic,
-        payload=message.payload.decode(),
-        qos=message.qos,
-    )
-    socketio.emit('mqtt_message', data=data)
-
-
-@mqtt.on_log()
-def handle_logging(client, userdata, level, buf):
-    print(level, buf)
-
-
+#서버 실행
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, use_reloader=False, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug = True)
--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
+export PYTHONPATH="/home/orangepi/IoT_target"
+export PYTHONIOENCODING=UTF-8
+cd $PYTHONPATH
+git pull
 chmod 775 /sys/class/gpio/export
 chmod 775 /sys/class/gpio/unexport
+git pull
 for gpio in "65" "68" "70" "71" "72" "73" "74" "76"; do
     GPIO_DIR=$(ls /sys/class/gpio/ | grep ${gpio})
     if [ -n "$GPIO_DIR" ]; then
@@ -12,7 +17,7 @@ for gpio in "65" "68" "70" "71" "72" "73" "74" "76"; do
     echo "out" > /sys/class/gpio/gpio${gpio}/direction
 done
 
-for gpio in "111" "112" "113" "114" "117" "118" "75"; do
+for gpio in "111" "112" "113" "114" "229" "117" "118" "75"; do
     GPIO_DIR=$(ls /sys/class/gpio/ | grep ${gpio})
     if [ -n "$GPIO_DIR" ]; then
             echo "GPIO already exist from past error"
@@ -23,3 +28,4 @@ for gpio in "111" "112" "113" "114" "117" "118" "75"; do
     sleep 1
     echo "in" > /sys/class/gpio/gpio${gpio}/direction
 done
+python3 app.py
