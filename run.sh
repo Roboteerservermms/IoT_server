f52578aab81d71db769f5570660b0b9ec5c5a217
kim <ing03201@gmail.com>
2021-10-05 18:04:44 +0900

    asdf

--- run.sh
+++ run.sh
@@ -30,4 +30,6 @@ for gpio in "111" "112" "113" "114" "229" "117" "118" "75"; do
 done
 timedatectl set-timezone Asia/Seoul
 python3 videoProcess.py &
+python3 manage.py makemigrations
+python3 manage.py migrate
 python3 manage.py runserver 0.0.0.0:8080
