fd0929b9a0b495b0f09aa9ccce9bcc6435e4f5c3
kim <ing03201@gmail.com>
2021-11-25 17:19:56 +0900

    fasdf

--- apps/app/controlVideo.py
+++ apps/app/controlVideo.py
@@ -3,6 +3,8 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 
+import re
+from typing_extensions import TypeVarTuple
 from django import template
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -10,12 +12,13 @@ from django.db.models.sql import query
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from django.utils import tree
 from .models import *
 from .constant import *
 from .mediaProvider import *
 from .video import VlcPlayer
 from vlc import EventType
-import requests, getmac, socket, json, threading, logging,time
+import requests, getmac, socket, json, threading, logging,time, asyncio
 from django.shortcuts import redirect
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
@@ -45,170 +48,183 @@ class videoThread(threading.Thread):
         threading.Thread.__init__(self)
         self.player = VlcPlayer('--mouse-hide-timeout=0')
         self.player.add_callback(EventType.MediaPlayerEndReached,self.videoEndHandler)
-        self.blackScreen = f"{settings.MEDIA_ROOT}/blackscreen.mp4"
-        self.nowPlay = ""
-        self.scheduleQ = Schedule.objects.none()
-        self.gpioQ = GPIOSetting.objects.none()
-        self.player.play(self.blackScreen)
+        self.blackScreenList = {"OUTPIN":[0,0,0,0,0,0,0] , "File":f"{settings.MEDIA_ROOT}/blackscreen.mp4","RTSP": None, "TTS": None}
+        self.playDict = {}
+        self.scheduleList = Schedule.objects.all().values()
+        self.gpioList = GPIOSetting.objects.all().values()
+        self.playListLock = threading.Lock()
+
+        self.highPin = 7
+
+        self.gpioOnState = False
         self.videoStopSig = False
         self.videoEndSig = False
-        self.scheduleRepeat = True
-        self.scheduleGpioRun = True
+        self.videoChangeSig = False
+
+    def chime(self,category, mediaId=None, gpioIn=None):
+        self.videoStopSig = True
+        if category == "Schedule":
+            self.videoStopSig = True
+            playList = self.scheduleListCheck(mediaId=mediaId)
+        elif category == "GPIOSetting":
+            self.videoStopSig = True
+            playList = self.gpioListCheck(mediaId=mediaId)
+
+
+    def gpioListCheck(self, mediaId=None, gpioIn=None):
+        self.playListLock.acquire()
+        retGpioDict = {}
+        try:
+            if mediaId:
+                for gpioDict in self.gpioList:
+                    if mediaId == gpioDict["id"]:
+                        retGpioDict = gpioDict
+                        break
+            elif gpioIn:
+                for gpioDict in self.gpioList:
+                    if gpioIn == gpioDict["IN"]:
+                        retGpioDict = gpioDict
+                        break
+            else:
+                lowGpioCount = 0
+                for pinNum, originNum in INPIN.items():
+                    inCommand = f"cat /sys/class/gpio/gpio{originNum}/value"
+                    retGPIOIN=subprocess.getoutput(inCommand)
+                    if str2bool(retGPIOIN):
+                        if pinNum == self.highPin:
+                            self.gpioOnState = True
+                            break
+                        elif pinNum < self.highPin:
+                            if pinNum == 0:
+                                self.videoStopSig = not self.videoStopSig
+                            self.highPin = pinNum
+                            self.gpioOnState = False
+                            break
+                        else:
+                            self.gpioOnState = False
+                            self.highPin = pinNum
+                            break
+                    else:
+                        lowGpioCount += 1
+
+                if lowGpioCount >= 7:
+                    self.highPin = 7
+                    return None
+                elif self.highPin == 0:
+                    return None
+                else:
+                    for gpioDict in self.gpioList:
+                        if self.highPin == gpioDict["IN"]:
+                            retGpioDict =  gpioDict
+        finally:
+            self.playListLock.release()
+            return retGpioDict
+    
+    def scheduleListCheck(self, mediaId=None):
+        self.playListLock.acquire()
+        retSchDict = {}
+        try:
+            if mediaId:
+                for scheduleDict in self.scheduleList:
+                    if mediaId == scheduleDict['id']:
+                        retSchDict = scheduleDict
+                        break
+            else:
+                now = datetime.datetime.now()
+                nowDay= now.weekday()
+                nowTime =  datetime.time(now.hour,now.minute)
+                for scheduleDict in self.scheduleList:
+                    if nowDay == scheduleDict['day']:
+                        if nowTime == scheduleDict["startTime"]:
+                            if scheduleDict["endTime"] == None:
+                                self.chime('Schedule',startTime=scheduleDict['startTime'])
+                                retSchDict = None
+                                break
+                            else:
+                                retSchDict = scheduleDict
+                        elif nowTime > scheduleDict['startTime']:
+                            if nowTime <= scheduleDict['endTime']:
+                                retSchDict = scheduleDict
+        finally:
+            self.playListLock.release()
+            return retSchDict
+
+    async def playLoop(self,media):
+        self.player.play(media)
+        self.videoEndSig = False
+        while not self.videoEndSig:
+            if self.videoStopSig:
+                self.player.stop()
+                break
+            elif self.videoChangeSig:
+                self.player.stop()
+                break
+            else:
+                pass
+    async def rtspPlayLoop(self,url):
+        self.player.play(url)
+        self.videoEndSig = False
+        while not self.videoEndSig:
+            if self.videoStopSig:
+                self.player.stop()
+                break
+            elif self.videoChangeSig:
+                self.player.stop()
+                break
+            elif not self.gpioOnState:
+                self.player.stop()
+                break
+            else:
+                pass
+
+    def classify(self,mediaDict,category=None):
+        if category == "Schedule":
+            for key, value in mediaDict:
+                if key == "IN":
+                    self.chime("GPIOSetting", gpioIn=value)
+        if key == "OUT":
+            for index, value in enumerate(value):
+                out_command = f'echo {value} > /sys/class/gpio/gpio{OUTPIN[index+1]}/value'
+                subprocess.getoutput(out_command)
+        elif key == "File":
+            if value != "":
+                loop = asyncio.get_event_loop()
+                loop.run_until_complete(self.playLoop(value))
+                loop.close()
+        elif key == "RTSP":
+            if value != "":
+                loop = asyncio.get_event_loop()
+                loop.run_until_complete(self.playLoop(value))
+                loop.close()
+        elif key == "TTS":
+            if value != "":
+                loop = asyncio.get_event_loop()
+                loop.run_until_complete(self.playLoop(TTS(value)))
+                loop.close()
+
+    def run(self):
+        while True:
+            if not self.videoStopSig:
+                schDict = self.scheduleListCheck()
+                gpioDict = self.gpioListCheck()
+                if schDict:
+                    self.classify(schDict, category="Schedule")
+                elif gpioDict:
+                    self.classify(gpioDict)
+                else:
+                    self.classify(self.blackScreenList)
+
+    def playListUpdate(self):
+        self.playListLock.acquire()
+        try:
+            self.scheduleList = list( Schedule.objects.all().values() )
+            self.gpioList = list( GPIOSetting.objects.all().values() )
+        finally:
+            self.playListLock.release()
+        logger.info("playList update Okay!")
 
     def videoEndHandler(self,event):
         self.videoEndSig = True
-    def pause(self):
-        self.videoStopSig = not self.videoStopSig
-        
-    def gpioRise(self, mediaId=None):
-        self.gpioQ = GPIOSetting.objects.none()
-        if mediaId:
-            self.gpioQ = GPIOSetting.objects.filter(
-                            Q(id = mediaId)
-                        )
-        else:
-            for pinNum, originNum in INPIN.items():
-                inCommand = f"cat /sys/class/gpio/gpio{originNum}/value"
-                retGPIOIN=subprocess.getoutput(inCommand)
-                if str2bool(retGPIOIN):
-                    if pinNum == 0:
-                        self.pause()
-                        break
-                    else:
-                        self.gpioQ = GPIOSetting.objects.filter(
-                            Q(IN = pinNum)
-                        )
-                        break
-
-
-    def play(self, media=None):
-        if media is not None:
-            self.nowPlay = media
-            self.player.play(media)
-            time.sleep(1.5)
-            duration = self.player.get_length() / 1000
-            time.sleep(duration)
-        if media is None:
-            self.player.play(self.blackScreen)
-            self.nowPlay = self.blackScreen
-
-    def playCategory(self):
-        if self.scheduleQ.exists():
-            self.schedulePlay()
-        elif self.gpioQ.exists():
-            self.gpioPlay()
-        else:
-            self.play()
-
-    def gpioPlay(self):
-        if self.gpioQ.exists():
-            logger.info(f"now play {self.gpioQ.values()[0].items()}")
-            for key, value in self.gpioQ.values()[0].items():
-                if key == "OUT":
-                    for index, value in enumerate(value):
-                        out_command = f'echo {value} > /sys/class/gpio/gpio{OUTPIN[index+1]}/value'
-                        subprocess.getoutput(out_command)
-                elif key == "File":
-                    if value != "":
-                        self.play(value)
-                elif key == "RTSP":
-                    if value != "":
-                        self.play(value)
-                elif key == "TTS":
-                    if value != "":
-                        self.play(TTS(value,settings.MEDIA_ROOT))
-
-    def schedulePlay(self):
-        if self.scheduleQ.exists():
-            logger.info(f"now play {self.scheduleQ.values()[0].items()}")
-            for key, value in self.scheduleQ.values()[0].items():
-                if key=="IN" and self.scheduleGpioRun:
-                    self.scheduleGpioRun = False
-                    self.gpioQ = GPIOSetting.objects.filter(
-                        Q(IN = value)
-                    )
-                    self.gpioPlay()
-                if key == "OUT":
-                    for index, value in enumerate(value):
-                        out_command = f'echo {value} > /sys/class/gpio/gpio{OUTPIN[index+1]}/value'
-                        subprocess.getoutput(out_command)
-                elif key == "File":
-                    if value != "":
-                        self.play(value)
-                elif key == "RTSP":
-                    if value != "":
-                        self.play(value)
-                elif key == "TTS":
-                    if value != "":
-                        self.play(TTS(value,settings.MEDIA_ROOT))
-
-    def scheduleAdd(self, mediaId=None):
-        if not mediaId:
-            now = datetime.datetime.now()
-            nowDay= now.weekday()
-            nowTime =  datetime.time(now.hour,now.minute)
-            try:
-                playOnceSchedule = Schedule.objects.filter(
-                    Q(day__contains = nowDay)
-                    & Q(startTime = nowTime)
-                    & Q(endTime__isnull=True)
-                )
-                playRepeatSchedule = Schedule.objects.filter(
-                        Q(day__contains = nowDay)
-                        & Q(startTime__lte = nowTime)
-                        & Q(endTime__gte = nowTime)
-                        & Q(endTime__isnull = False)
-                )
-                if playRepeatSchedule.exists():
-                    self.scheduleQ = playRepeatSchedule
-                elif playOnceSchedule.exists():
-                    if self.scheduleQ != playOnceSchedule:
-                        if self.scheduleQ == Schedule.objects.none():
-                            if self.scheduleRepeat:
-                                self.scheduleQ = playOnceSchedule
-                                self.scheduleRepeat = False
-                            else:
-                                self.scheduleQ = Schedule.objects.none()
-                        else:
-                            self.scheduleQ = playOnceSchedule
-                            self.scheduleRepeat = False
-                    else:
-                        self.scheduleQ = Schedule.objects.none()
-                else:
-                    self.scheduleGpioRun = True
-                    self.scheduleRepeat = True
-                    self.scheduleQ = Schedule.objects.none()
-
-            except Schedule.DoesNotExist:
-                pass
-        else:
-            try:
-                self.scheduleQ = Schedule.objects.filter(
-                    Q(id=mediaId)
-                )
-            except Schedule.DoesNotExist:
-                pass
-
-    def chime(self,category, mediaId):
-        if category == "Schedule":
-            self.player.pause()
-            self.scheduleAdd(mediaId=mediaId)
-            self.schedulePlay()
-        elif category == "GPIOSetting":
-            self.player.pause()
-            self.gpioRise(mediaId=mediaId)
-            self.gpioPlay()
-
-    def run(self):
-        self.blackScreen = f"{settings.MEDIA_ROOT}/blackscreen.mp4"
-        self.nowPlay = self.blackScreen
-        self.player.play(self.blackScreen)
-        while True:
-            if not self.videoStopSig:
-                self.scheduleAdd()
-                self.gpioRise()
-                self.playCategory()
-
 
 
 @method_decorator(csrf_exempt, name="dispatch")
